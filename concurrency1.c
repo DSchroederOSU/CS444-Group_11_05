7096e2f214d8f164dcf02c58d8924f903281e867
DSchroederOSU <schrodan@oregonstate.edu>
2017-04-19 15:47:46 -0700

    DONE I THINK

--- concurrency1.c
+++ concurrency1.c
@@ -24,8 +24,9 @@ int rdrand64_step (uint64_t *arand){
 	return (int) ok;
 }
 
-
 pthread_mutex_t my_mutex;
+pthread_cond_t full;
+pthread_cond_t empty;
 
 //this is the struct for the items in the buffer
 struct buffer_item{
@@ -46,34 +47,65 @@ int get_random_number(int min, int max);
 int rdrand_flag;
 int check_buffer_open();
 int check_buffer_for_item();
+int ccounter=0;
+int pcounter=0;
 
-int main(){
+int main( int argc, char *argv[]){
 	
-	//intitallize buffer
-	struct buffer_item empty;
-	empty.num = -1;
-	empty.wait = -1;
+	if(argc > 2){ 
+		printf("Usage: [executable] [(int) NumThreads]\n");
+		return 1;
+	}
+	
+	int number_of_threads = atoi( argv[1] );
+
+	printf("before arrays number of threads is: %d\n", number_of_threads);	
+	pthread_t *prod_threads;
+	prod_threads = malloc(sizeof(pthread_t)*number_of_threads);
+	pthread_t *cons_threads;
+	cons_threads = malloc(sizeof(pthread_t)*number_of_threads);
+	printf("after arrays\n");	
+	
 	int i;
+		
+	//intitallize buffer
+	struct buffer_item none;
+	none.num = -1;
+	none.wait = -1;
+	
 	for(i = 0; i < MAX_SIZE; i++){
-		buffer[i] = empty;
+		buffer[i] = none;
 	}
 	
 	//check to make sure rdrand is supported 
 	if(check_rdrand() == 1) rdrand_flag = 1;
 
-	pthread_t producer_thread, consumer_thread;
-
+	
 	pthread_mutex_init(&my_mutex, NULL);
 	
+	pthread_cond_init(&full, NULL);
+	pthread_cond_init(&empty, NULL);
 
-	pthread_create(&producer_thread, NULL, produce, NULL);
-	pthread_create(&consumer_thread, NULL, consume, NULL);
+	printf("REAHCED\n")	;
+	for(i = 0; i < number_of_threads; i++){
 	
+		//pthread_t producer_thread, consumer_thread;
+		//prod_threads[i] = producer_thread;
+		//cons_threads[i] = consumer_thread;
+		pthread_create(&prod_threads[i], NULL, produce, NULL);
+		printf("THIS IS PRINTING\n");
+		pthread_create(&cons_threads[i], NULL, consume, NULL);
 
-	pthread_join(producer_thread, NULL);
-	pthread_join(consumer_thread, NULL);
+		printf("REACHED\n");
+	}
 
+	for(i = 0; i < number_of_threads; i++){
+		pthread_join(prod_threads[i], NULL);
+		pthread_join(cons_threads[i], NULL);
+	}
 	
+		
+			
 	return 0;
 }	
 
@@ -81,24 +113,24 @@ int main(){
 //then add item to buffer
 //if buffer is full, block until consumer removes an item
 void* produce(void *data){
-	int i;
-	for(i = 0; i < 10; i++){
-		int wait_to_produce = get_random_number(3,7);
-		
-		printf("PRODUCER: Sleeping for %d seconds\n", wait_to_produce);	
-		sleep(wait_to_produce);	
-		
-		int buffer_check = check_buffer_open();	
-		while(buffer_check == -1){
-
-			buffer_check = check_buffer_open();	
-		}
-		pthread_mutex_lock(&my_mutex);
-		struct buffer_item tmp = make_item();
-		printf("PRODUCER: addinf item with num: %d and wait: %d\n", tmp.num, tmp.wait);
-		buffer[buffer_check] = tmp;	
-		pthread_mutex_unlock(&my_mutex);	
+	int num = pcounter;
+	pcounter++;	
+	int wait_to_produce = get_random_number(3,7);
+	
+	printf("PRODUCER%d: Sleeping for %d seconds\n",num, wait_to_produce);	
+	sleep(wait_to_produce);	
+	
+	pthread_mutex_lock(&my_mutex);
+	int buffer_check = check_buffer_open();
+	while(buffer_check == -1){
+		pthread_cond_wait(&full, &my_mutex);	
+		buffer_check = check_buffer_open();	
 	}
+	struct buffer_item tmp = make_item();
+	printf("PRODUCER%d: adding item with num: %d and wait: %d\n", num, tmp.num, tmp.wait);
+	buffer[buffer_check] = tmp;	
+	pthread_cond_signal(&empty);
+	pthread_mutex_unlock(&my_mutex);	
 	pthread_exit(0);
 }	
 
@@ -107,21 +139,24 @@ void* produce(void *data){
 //if buffer is empty, block until producer adds an item
 void* consume(void *data){
 
-	int i;
-	for(i = 0; i < 10; i++){
-		int buffer_check = check_buffer_for_item();
-		while(buffer_check == -1){
-			
-			buffer_check = check_buffer_for_item();
-		}
-		pthread_mutex_lock(&my_mutex);
-			printf("CONSUMER: consuming number %d\n", buffer[buffer_check].num);
-			buffer[buffer_check].num = -1;
-			printf("CONSUMER: consuming for %d seconds\n", buffer[buffer_check].wait);
-			sleep(buffer[buffer_check].wait);	
-		pthread_mutex_unlock(&my_mutex);	
+	int num = ccounter;
+	ccounter++;
+	pthread_mutex_lock(&my_mutex);
+	int buffer_check = check_buffer_for_item();
+	while(buffer_check == -1){
+		pthread_cond_wait(&empty, &my_mutex);
+		buffer_check = check_buffer_for_item();
 	}
-	pthread_exit(0);
+	int temp = buffer[buffer_check].num;
+	int time = buffer[buffer_check].wait;
+	buffer[buffer_check].num = -1;
+	
+	pthread_cond_signal(&full);
+	pthread_mutex_unlock(&my_mutex);
+	printf("CONSUMER%d: waiting %d seconds to consume %d\n", num, time, temp);
+	sleep(buffer[buffer_check].wait);	
+	printf("CONSUMER%d: consuming number %d\n", num, temp);
+	pthread_exit(0);	
 }
 
 struct buffer_item make_item(){
