4126204c49cd2e10090eb4d6bd3183bc23c94648
DSchroederOSU <schrodan@oregonstate.edu>
2017-04-19 09:42:43 -0700

    working on concurrency1.c. made a skeleton of the project

--- concurrency1.c
+++ concurrency1.c
@@ -4,10 +4,41 @@
    Spring 2017
 */
 
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_SIZE 32
+
+struct buffer_item{
+	int num; //this is the number to print	
+	int wait; //this is the number of seconds to wait
+};
+
+struct buffer_item buffer[MAX_SIZE];
+
+//functions
+void produce();
+void consume();
 
 int main(){
 
 
 
 	return 0;
-}		
+}	
+
+//wait a random amount of time 3-7 seconds,
+//then add item to buffer
+//if buffer is full, block until consumer removes an item
+void produce(){
+	
+}
+
+//Sleep for the amount of time in buffer_item->wait
+//then print the number in buffer_item->num
+//if buffer is empty, block until producer adds an item
+void consume(){
+
+}	
