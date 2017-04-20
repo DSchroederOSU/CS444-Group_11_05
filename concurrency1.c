ce1d4cd2a712b1dfed27d304539fd552d15c4c6c
DSchroederOSU <schrodan@oregonstate.edu>
2017-04-20 15:43:39 -0700

    added second random generator

--- concurrency1.c
+++ concurrency1.c
@@ -10,8 +10,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <immintrin.h>
+#include <stdint.h>
 
 #define MAX_SIZE 32
+#define N 624
+#define M 397
+#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
+#define UPPER_MASK 0x80000000UL /* most significant w-r bits */
+#define LOWER_MASK 0x7fffffffUL /* least significant r bits */
+
+
 
 //this calls assembler instruction rdrand
 //and places the random number value into *arand
@@ -48,12 +56,20 @@ int rdrand_flag;
 int check_buffer_open();
 int check_buffer_for_item();
 void print_buf();
+unsigned long genrand_int32(void);
 int ccounter=0;
 int pcounter=0;
 int buffer_size = 0; 
+static unsigned long mt[N];
+static int mti=N+1;
 
 int main( int argc, char *argv[]){
-	
+
+	int k;
+	for(k = 0; k < 20; k++){
+		printf("Random number: %d\n", get_random_number(2, 9));
+	}
+	return 0;	
 	if(argc > 2){ 
 		printf("Usage: [executable] [(int) NumThreads]\n");
 		return 1;
@@ -165,24 +181,31 @@ struct buffer_item make_item(){
 
 
 int get_random_number(int min, int max){
-
-	uint64_t arand;
-	//retry for 10 times in case rdrand was inturrupted
-	int success = 1;
-	int attempt_limit_exceeded = -1;
-	int attempt_limit = 10;
-	int i;
-	for(i = 0; i < attempt_limit; i++){
-		//if rdrand64_step returned a usable random value
-		if(rdrand64_step(&arand)){
-			if(min == -1 && max == -1)
-				return (int) arand;
-			else
-				return (arand % (uint64_t)(max - min + 1)) + min;
-		}  
+	if(check_rdrand() == 1){
+		uint64_t arand;
+		//retry for 10 times in case rdrand was inturrupted
+		int success = 1;
+		int attempt_limit_exceeded = -1;
+		int attempt_limit = 10;
+		int i;
+		for(i = 0; i < attempt_limit; i++){
+			//if rdrand64_step returned a usable random value
+			if(rdrand64_step(&arand)){
+				if(min == -1 && max == -1)
+					return (int) arand;
+				else
+					return (arand % (uint64_t)(max - min + 1)) + min;
+			}  
+		}
+		//this will return -1 if rdrand failed
+		return attempt_limit_exceeded;
 	}
-	//this will return -1 if rdrand failed
-	return attempt_limit_exceeded;
+	else{//use mt19937
+		printf("USING OTHER RANDOM\n");
+		int rand = (int)genrand_int32();
+		printf("RANDOM NUMBER IS: %d\n", rand);
+		return rand;
+	}
 	
 				
 }//end get_random_number
@@ -234,4 +257,78 @@ void print_buf(){
 	for(i = 0; i < MAX_SIZE; i++){
 		printf("Buffer[%d]: num: %d wait: %d\n", i, buffer[i].num, buffer[i].wait);
 	}
+}
+
+void init_genrand(unsigned long s){
+	mt[0]= s & 0xffffffffUL;
+	for (mti=1; mti<N; mti++) {
+		mt[mti] = 
+			(1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
+		/* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+		/* In the previous versions, MSBs of the seed affect   */
+		/* only MSBs of the array mt[].                        */
+		/* 2002/01/09 modified by Makoto Matsumoto             */
+		mt[mti] &= 0xffffffffUL;
+		/* for >32 bit machines */
+	}	
+}
+
+void init_by_array(unsigned long init_key[], int key_length){
+	int i, j, k;
+	init_genrand(19650218UL);
+	i=1; j=0;
+	k = (N>key_length ? N : key_length);
+	for (; k; k--) {
+		mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL))
+			+ init_key[j] + j; /* non linear */
+		mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
+		i++; j++;
+		if (i>=N) { mt[0] = mt[N-1]; i=1; }
+		if (j>=key_length) j=0;
+	}
+	for (k=N-1; k; k--) {
+		mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL))
+			- i; /* non linear */
+		mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
+		i++;
+		if (i>=N) { mt[0] = mt[N-1]; i=1; }
+	}
+
+	mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
+}
+unsigned long genrand_int32(void)
+{
+	unsigned long y;
+	static unsigned long mag01[2]={0x0UL, MATRIX_A};
+	/* mag01[x] = x * MATRIX_A  for x=0,1 */
+
+	if (mti >= N) { /* generate N words at one time */
+		int kk;
+
+		if (mti == N+1)   /* if init_genrand() has not been called, */
+			init_genrand(5489UL); /* a default initial seed is used */
+
+		for (kk=0;kk<N-M;kk++) {
+			y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+			mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+		}
+		for (;kk<N-1;kk++) {
+			y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
+			mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+		}
+		y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
+		mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
+
+		mti = 0;
+	}
+  
+	y = mt[mti++];
+
+	/* Tempering */
+	y ^= (y >> 11);
+	y ^= (y << 7) & 0x9d2c5680UL;
+	y ^= (y << 15) & 0xefc60000UL;
+	y ^= (y >> 18);
+
+	return y;
 }
