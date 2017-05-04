f1cc0bee6c166599c52da86c7462076456200697
Luke Morrison <morriluk@oregonstate.edu>
2017-05-04 11:46:59 -0700

    cleanup + fix issues

--- Assignment2/concurrency2.c
+++ Assignment2/concurrency2.c
@@ -18,18 +18,21 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-volatile int forks[5];
+int forks[5];
+int eating[5];
+
 char * names[5] = {"Albert", "Beaufort", "Charles", "David", "Elliot"};
 pthread_mutex_t p_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 int counter;
+
 struct thread_info {
 	int right;
 	int left;
 	int name;
+	int eating;
 };
 
-int counter;
-int eating[5];
 void print_forks ()
 {
 	pthread_mutex_lock(&p_mutex);
@@ -40,6 +43,7 @@ void print_forks ()
 }
 void print_status()
 { 
+	pthread_mutex_lock(&p_mutex);
 	printf("--------Round %d---------\n", counter);
 	
 	int i;
@@ -57,67 +61,53 @@ void print_status()
 		else
 			printf("%s: %s\n", names[i], "Finished");
 	}
-	
+	pthread_mutex_unlock(&p_mutex);	
 	
 }
-void philosopher_func (struct thread_info * p)
+void * philosopher_func (void * p_void)
 {
-	int index;
-	int i;
-	for(i = 0; i < 5; i++){
-		if(p->name == 0)
-			index = 0;
-		else if(p->name == 1)
-			index = 1;
-		else if(p->name == 2)
-			index = 2;
-		else if(p->name == 3)
-			index = 3;
-		else
-			index = 4;
-	}
-	
+	struct thread_info *p = (struct thread_info *)p_void;
 	int thinktime, eattime;
-	counter = 1;
 	 
 
-		//THINK
-		thinktime = (rand() % (THINK_MAX-THINK_MIN+1)) + THINK_MIN;	
-		printf("%s is starting to THINK for %d seconds.\n", names[p->name], thinktime);
-		sleep(thinktime);
+	//THINK
+	thinktime = (rand() % (THINK_MAX-THINK_MIN+1)) + THINK_MIN;	
+	printf("%s is starting to THINK for %d seconds.\n", names[p->name], thinktime);
+	sleep(thinktime);
 
-		//GET FORK
-		printf("%s is done thinking. Getting forks...\n", names[p->name]);
-		while (1) {
-			pthread_mutex_lock(&p_mutex);
-			if (forks[p->left] == 0 && forks[p->right] == 0) {
-				forks[p->left] = 1;
-				forks[p->right]= 1;
-				eating[index] = 1;
-				pthread_mutex_unlock(&p_mutex);
-				break;
-			}
-			else {
-				pthread_mutex_unlock(&p_mutex);
-				//sleep(50); //maybe?
-			}
-		}
-		 
-		//EAT
-		eattime = (rand() % (EAT_MAX-EAT_MIN+1)) + EAT_MIN;	
-		printf("%s is starting to EAT for %d seconds.\n", names[p->name], eattime);
-		sleep(eattime);
-
-		//PUT FORK
-		eating[index] = 2;
-		printf("%s is done eating. Putting down forks...\n", names[p->name]);
+	//GET FORK
+	printf("%s is done thinking. Getting forks...\n", names[p->name]);
+	while (1) {
 		pthread_mutex_lock(&p_mutex);
-		forks[p->left] = 0;
-		forks[p->right]= 0;
-		pthread_mutex_unlock(&p_mutex);
-		print_status();
-		counter++;
+		if (forks[p->left] == 0 && forks[p->right] == 0) {
+			forks[p->left] = 1;
+			forks[p->right]= 1;
+			eating[p->name] = 1;
+			pthread_mutex_unlock(&p_mutex);
+			break;
+		}
+		else {
+			pthread_mutex_unlock(&p_mutex);
+			//sleep(50); //maybe?
+		}
+	}
 	 
+	//EAT
+	eattime = (rand() % (EAT_MAX-EAT_MIN+1)) + EAT_MIN;	
+	printf("%s is starting to EAT for %d seconds.\n", names[p->name], eattime);
+	sleep(eattime);
+
+	//PUT FORK
+	eating[p->name] = 2;
+	printf("%s is done eating. Putting down forks...\n", names[p->name]);
+	pthread_mutex_lock(&p_mutex);
+	forks[p->left] = 0;
+	forks[p->right]= 0;
+	pthread_mutex_unlock(&p_mutex);
+	print_status();
+	counter++;
+
+	return NULL; 
 }
 
 int main()
@@ -127,6 +117,7 @@ int main()
 	struct thread_info p[5];
 
 	srand(time(NULL));
+	counter = 1;
 
 	for (int i = 0; i < 5; i++) {
 		forks[i] = 0;
@@ -138,7 +129,7 @@ int main()
 		else
 			p[i].right = 0;
 
-		pthread_create(&thread_ids[i], NULL, (void *) philosopher_func,(void *) &p[i]);
+		pthread_create(&thread_ids[i], NULL, philosopher_func,(void *) &p[i]);
 	}
 
 	for (int i = 0; i < 5; i++)
