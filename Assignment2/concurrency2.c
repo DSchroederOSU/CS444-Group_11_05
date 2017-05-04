fe2fee9cb7be6a8dc4f24a05b72d855ab6f14d2e
DSchroederOSU <schrodan@oregonstate.edu>
2017-05-04 11:21:19 -0700

    added tex and make

--- Assignment2/concurrency2.c
+++ Assignment2/concurrency2.c
@@ -1,52 +1,149 @@
-/*
- * Recitation Concurrency Assignment
- * Operating Systems II
- * Brian Ozarowicz, Daniel Schroeder
- * Group 11-05
- * Spring 2017
- */
-
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <string.h>
 
-pthread_t philosopher0;
-pthread_t philosopher1;
-pthread_t philosopher2;
-pthread_t philosopher3;
-pthread_t philosopher4;
-pthread_mutex_t forks[5];
+#define THINK_MIN 1
+#define THINK_MAX 20
+#define EAT_MIN 2
+#define EAT_MAX 9
 
-void *waiter(int n)
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+volatile int forks[5];
+char * names[5] = {"Albert", "Beaufort", "Charles", "David", "Elliot"};
+pthread_mutex_t p_mutex = PTHREAD_MUTEX_INITIALIZER;
+int counter;
+struct thread_info {
+	int right;
+	int left;
+	int name;
+};
+
+int counter;
+int eating[5];
+void print_forks ()
 {
-        printf("Philosopher %d is thinking\n", n);
-        pthread_mutex_lock(&forks[n]);
-        pthread_mutex_lock(&forks[(n+1)%5]);
-        printf("Philosopher %d is eating\n", n);
-        sleep(2);
-        pthread_mutex_unlock(&forks[n]);
-        pthread_mutex_unlock(&forks[(n+1)%5]);
-        printf("Philosopher %d is done eating\n", n);
-        return NULL;
+	pthread_mutex_lock(&p_mutex);
+	printf(ANSI_COLOR_GREEN "%d %s %d %s %d %s %d %s %d %s\n" ANSI_COLOR_RESET,
+		forks[0], names[0], forks[1], names[1], forks[2],
+		names[2],forks[3], names[3], forks[4], names[4]);
+	pthread_mutex_unlock(&p_mutex);
+}
+void print_status()
+{ 
+	printf("--------Round %d---------\n", counter);
+	
+	int i;
+	for(i = 0; i < 5; i++){
+		if(forks[i] == 1)
+			printf("Fork %d: %s\n", i, "Taken");
+		else
+			printf("Fork %d: %s\n", i, "Available");
+	}
+	for(i = 0; i < 5; i++){
+		if(eating[i] == 1)
+			printf("%s: %s\n", names[i], "Eating");
+		else if(eating[i] == 0)
+			printf("%s: %s\n", names[i], "Waiting");
+		else
+			printf("%s: %s\n", names[i], "Finished");
+	}
+	
+	
+}
+void philosopher_func (struct thread_info * p)
+{
+	int index;
+	int i;
+	for(i = 0; i < 5; i++){
+		if(p->name == 0)
+			index = 0;
+		else if(p->name == 1)
+			index = 1;
+		else if(p->name == 2)
+			index = 2;
+		else if(p->name == 3)
+			index = 3;
+		else
+			index = 4;
+	}
+	
+	int thinktime, eattime;
+	counter = 1;
+	 
+
+		//THINK
+		thinktime = (rand() % (THINK_MAX-THINK_MIN+1)) + THINK_MIN;	
+		printf("%s is starting to THINK for %d seconds.\n", names[p->name], thinktime);
+		sleep(thinktime);
+
+		//GET FORK
+		printf("%s is done thinking. Getting forks...\n", names[p->name]);
+		while (1) {
+			pthread_mutex_lock(&p_mutex);
+			if (forks[p->left] == 0 && forks[p->right] == 0) {
+				forks[p->left] = 1;
+				forks[p->right]= 1;
+				eating[index] = 1;
+				pthread_mutex_unlock(&p_mutex);
+				break;
+			}
+			else {
+				pthread_mutex_unlock(&p_mutex);
+				//sleep(50); //maybe?
+			}
+		}
+		 
+		//EAT
+		eattime = (rand() % (EAT_MAX-EAT_MIN+1)) + EAT_MIN;	
+		printf("%s is starting to EAT for %d seconds.\n", names[p->name], eattime);
+		sleep(eattime);
+
+		//PUT FORK
+		eating[index] = 2;
+		printf("%s is done eating. Putting down forks...\n", names[p->name]);
+		pthread_mutex_lock(&p_mutex);
+		forks[p->left] = 0;
+		forks[p->right]= 0;
+		pthread_mutex_unlock(&p_mutex);
+		print_status();
+		counter++;
+	 
 }
 
 int main()
 {
-        int i;
-        pthread_mutex_init(&forks[0], NULL);
-        pthread_mutex_init(&forks[1], NULL);
-        pthread_mutex_init(&forks[2], NULL);
-        pthread_mutex_init(&forks[3], NULL);
-        pthread_mutex_init(&forks[4], NULL);
-        pthread_create(&philosopher0, NULL, (void *)waiter, (void *)0);
-        pthread_create(&philosopher1, NULL, (void *)waiter, (void *)1);
-        pthread_create(&philosopher2, NULL, (void *)waiter, (void *)2);
-        pthread_create(&philosopher3, NULL, (void *)waiter, (void *)3);
-        pthread_create(&philosopher4, NULL, (void *)waiter, (void *)4);
-        pthread_join(philosopher0, NULL);
-        pthread_join(philosopher1, NULL);
-        pthread_join(philosopher2, NULL);
-        pthread_join(philosopher3, NULL);
-        pthread_join(philosopher4, NULL);
-        return 0;
+	memset(eating, 0, sizeof(int)*5);
+	pthread_t thread_ids[5];
+	struct thread_info p[5];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < 5; i++) {
+		forks[i] = 0;
+		p[i].name = i;
+
+		p[i].left = i;
+		if (i < 4)
+			p[i].right = i+1;
+		else
+			p[i].right = 0;
+
+		pthread_create(&thread_ids[i], NULL, (void *) philosopher_func,(void *) &p[i]);
+	}
+
+	for (int i = 0; i < 5; i++)
+		pthread_join(thread_ids[i], NULL);
+
+	return 0;
 }
+
